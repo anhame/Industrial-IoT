d9286a903eb9f537e935790c7d142517e1d8d171
Marc Schier <marcschier@hotmail.com>
2019-09-04 21:16:04 +0200

    versions

--- tools/getversion.ps1
+++ tools/getversion.ps1
@@ -11,14 +11,9 @@
 & dotnet @("tool", "install", "-g", "nbgv") 2>&1 | Out-Null
 
 # Call tool for cloud build
-& nbgv  @("cloud")
+& nbgv  @("cloud", "-c", "-a")
 if ($LastExitCode -ne 0 -and $LastExitCode -ne 5) {
     throw "Error: 'nbgv' failed with $($LastExitCode)."
 }
 
-$result = @{}
-(& nbgv  @("get-version")) | ForEach-Object {
-    $key, $value = $_.Split(':')
-    $result[$key.Trim()] = $value.Trim()
-}
-return $result
+return ((& nbgv  @("get-version", "-f", "json")) | ConvertFrom-Json).CloudBuildAllVars
